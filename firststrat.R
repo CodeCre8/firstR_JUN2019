1599b9e7aecdbf91bdee969e2e865c6dc04c27e5
Quilt9 <developer@quilt9digital.com>
2019-06-16 22:48:40 -0500

    Finished creating and adding indicators to strategy.

--- firststrat.R
+++ firststrat.R
@@ -10,6 +10,11 @@ library(quantstrat)
 library(TTR)
 library(IKTrading)
 library(blotter)
+
+
+# ===============================================================
+# Testing
+# ===============================================================
 # The closing price of SPY over the past 4.5 years.
 getSymbols("SPY",
            from = "2014-01-01",
@@ -28,7 +33,9 @@ plot(Cl(SPY))
 lines(SMA(Cl(SPY), n = 200), col = "blue")
 
 
+
 # Setting up Trading Strategy
+
 
 # ===============================================================
 # 1. Define Initialization Settings
@@ -71,6 +78,7 @@ account.st <- "firststrat"
 # 1.5 Remove the existing strategy if there is one
 rm.strat(strategy.st)
 
+
 # ===============================================================
 # 2. Initialization - portfolio, account, order and strategy
 # ===============================================================
@@ -85,6 +93,112 @@ initOrders(portfolio.st, initDate = initdate)
 strategy(strategy.st, store = TRUE)
 
 
+# ===============================================================
+# 3. Create and Add 5 indicators for trading strategy
+# ===============================================================
+# This strategy will incorporate a combination of 
+# basic moving average crossover and RSI as an oscillation indicator.
+#
+# SMA - Simple Moving Average
+# ===============================================================
+# A fast moving average with a slower moving average is a simple 
+# and standard way to predict when prices in an asset are expected 
+# to rise in the future. While a single indicator can provide 
+# a lot of information, a truly robust trading system requires 
+# multiple indicators to operate effectively.
+# ===============================================================
+# 3.1 Add a 200-day SMA indicator to strategy.st
+add.indicator(strategy = strategy.st,
+              # Add the SMA function
+              name = "SMA",
+              # Create a lookback period
+              arguments = list(x = quote(Cl(mktdata)),
+                               n = 200),
+              label = "SMA200"
+)
+# 3.2 Add a more robust indicator 50-day SMA to strategy.st
+add.indicator(strategy = strategy.st,
+              name = "SMA",
+              arguments = list(x = quote(Cl(mktdata)),
+                               n = 50),
+              label = "SMA50")
+
+# RSI - Relative Strength Indicator
+# ===============================================================
+# The RSI can predict when a price has sufficiently pulled back, 
+# especially with a short period such as 2 or 3 trading days.
+# Indicator that rises with positive price changes
+# and falls with negative price changes. 
+# It is equal to 100 - 100 / (1 + RS), where RS is the 
+# average gain over average loss over the lookback period. 
+# At various lengths, this indicator can range from a 
+# reversion indicator, to a trend filter, or anywhere in between. 
+# There are various ways of computing the RSI.
+# ===============================================================
+# 3.3 Add a RSI indicator with a 3-day lookback period
+add.indicator(strategy = strategy.st,
+              name = "RSI",
+              arguments = list(price = quote(Cl(mktdata)),
+                               n = 3),
+              label = "RSI_3")
+# 3.4 Use a function to add a 3.5 day RSI by averaging 3 and 4 day RSI
+calc_RSI_avg <- function(price, n1, n2) {
+  # RSI 1 takes an input of the price and n1
+  RSI_1 <- RSI(price = price, n = n1)
+  # RSI 2 takes an input of the price and n2
+  RSI_2 <- RSI(price = price, n = n2)
+  # RSI_avg is the avg of RSI_1 and RSI_2
+  RSI_avg <- (RSI_1 + RSI_2) / 2
+  # The output of RSI_avg needs a column
+  column(price) <- "RSI_avg"
+  # Return price
+  return(price)
+}
+# 3.4.1 Add this function calc_RSI_avg to strategy.st
+# as RSI_3_4 with n1 = 3 and n2 = 4
+add.indicator(strategy = strategy.st,
+              name = calc_RSI_avg,
+              arguments = list(price = quote(Cl(mktdata)),
+                               n1 = 3, n2 = 4), 
+              label = "RSI_3_4")
+
+# David Varadi Oscillator - DVO
+# ===============================================================
+# The purpose of the DVO is similar to something like the RSI 
+# in that it attempts to find opportunities to buy a temporary dip 
+# and sell in a temporary uptrend. In addition to obligatory market data, 
+# an oscillator function takes in two lookback periods.
+#
+# First, the function computes a ratio between the closing price 
+# and average of high and low prices. Next, it applies an SMA 
+# to that quantity to smooth out noise, usually on a very small time frame, 
+# such as two days. Finally, it uses the runPercentRank() function 
+# to take a running percentage rank of this average ratio, and 
+# multiplies it by 100 to convert it to a 0-100 quantity.
+#
+# **Note: High Low Close (HLC) is a built in function 
+# used to obtain the High, Low, and Closing Price of an asset 
+# on a given trading day.
+# ===============================================================
+# 3.5 # Declare the DVO function 
+# with a 2-day time frame and a lookback period of 126
+DVO <- function(HLC, navg = 2, percentlookback = 126) {
+  # Compute the ratio between closing prices to the average of high and low
+  ratio <- Cl(HLC) / ((Hi(HLC) + Lo(HLC)) / 2 )
+  # Use SMA to smooth out the ratio output - avgratio
+  avgratio <- SMA(ratio, n = navg)
+  # Convert ratio into a 0-100 value using runPercentRank() - out
+  out <- runPercentRank(avgratio, n = percentlookback, exact.multiplier = 1) * 100
+  # The output of out needs a column
+  colnames(out) <- "DVO"
+  # Return out
+  return(out)
+}
+# 3.5.1 Add indicator DVO to strategy.st
+add.indicator(strategy = strategy.st,
+              name = "DVO",
+              arguments = list(HLC = quote(HLC(mktdata)), navg = 2, percentlookback = 126),
+              label = "DVO_2_126")
 
 
 
